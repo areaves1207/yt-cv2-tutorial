18129920d61750d58c6b764dc47e6274dc58708e
areaves1207 <areaves0816@gmail.com>
2026-06-04 04:46:09 +0200

    init commit project 3 ppe detector

---
+++ Project3-PPEDetection/PPE-Detector.py
@@ -0,0 +1,101 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+from sort import *
+
+cap = cv2.VideoCapture("../Videos/ppe-1.mp4") #for video
+
+
+model = YOLO("../Yolo-Weights/yolov8n.pt")
+
+classNames = ["person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+              "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe",
+              "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", 
+              "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", 
+              "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", 
+              "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", 
+              "teddy bear", "hair drier", "toothbrush"]
+
+mask = cv2.imread("./mask.png")
+limitsUp = [103, 161, 296, 161] #coors of the crossing line
+limitsDown = [527, 416, 730, 416] #coors of the crossing line
+
+
+#tracking
+tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+totalCountUp = []
+totalCountDown = []
+
+while True:
+
+    success, img = cap.read()
+    imgRegion = cv2.bitwise_and(img, mask)
+
+    graphics = cv2.imread("./graphics.png", cv2.IMREAD_UNCHANGED)
+    img = cvzone.overlayPNG(img, graphics, (730, 260))
+    results = model(imgRegion, stream=True)
+
+    detections = np.empty((0, 5))
+
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+
+            #bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # x1, y1, w, h = box.xywh[0]
+            w, h = x2-x1, y2-y1
+            bbox = (int(x1), int(y1), int(w), int(h))
+
+            #Confidence
+            conf = math.ceil((box.conf[0] * 100)) / 100
+
+            #class name
+            cls = int(box.cls[0])
+            currentClass = classNames[cls]
+
+            if(currentClass == "person") and conf > 0.3:
+                # cvzone.cornerRect(img, bbox, l=9, rt=5)
+                # cvzone.putTextRect(img, f'{currentClass} {conf}', (max(0,x1), max(35,y1)), scale=0.6, thickness=1, offset=3)
+                currentArray = np.array([x1, y1, x2, y2, conf])
+                detections = np.vstack((detections, currentArray))
+
+    resultsTracker = tracker.update(detections)
+    cv2.line(img, (limitsUp[0], limitsUp[1]), (limitsUp[2], limitsUp[3]), (0, 0, 255), 5)
+    cv2.line(img, (limitsDown[0], limitsDown[1]), (limitsDown[2], limitsDown[3]), (0, 0, 255), 5)
+
+    
+    for result in resultsTracker:
+        x1, y1, x2, y2, id = result
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        w, h = x2-x1, y2-y1
+        print(result)
+        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255,0,0))
+        cvzone.putTextRect(img, f'{int(id)}', (max(0,x1), max(35,y1)), scale=2, thickness=3, offset=10)
+
+
+        cx, cy = x1 + w//2, y1 + h//2
+        cv2.circle(img, (cx, cy), 5, (255,0,255), cv2.FILLED)
+
+        if limitsUp[0] < cx < limitsUp[2] and limitsUp[1] - 15 < cy < limitsUp[3] + 15:
+            if totalCountUp.count(id) == 0:
+                totalCountUp.append(id)
+                cv2.line(img, (limitsUp[0], limitsUp[1]), (limitsUp[2], limitsUp[3]), (0, 255, 0), 5)
+
+        if limitsDown[0] < cx < limitsDown[2] and limitsDown[1] - 15 < cy < limitsDown[3] + 15:
+            if totalCountDown.count(id) == 0:
+                totalCountDown.append(id)
+                cv2.line(img, (limitsDown[0], limitsDown[1]), (limitsDown[2], limitsDown[3]), (0, 255, 0), 5)
+
+                
+
+    cv2.putText(img, str(len(totalCountUp)), (929, 345), (0,255,0), 7)
+    cv2.putText(img, str(len(totalCountDown)), (1191, 345), (0,255,0), 7)
+
+    cv2.imshow("Image", img)
+    # cv2.imshow("Image", imgRegion)
+    cv2.waitKey(1)
