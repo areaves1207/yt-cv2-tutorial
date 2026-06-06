0d5a87f6d2ac8678b985ec06c7c314943586f7fc
areaves1207 <areaves0816@gmail.com>
2026-06-07 00:04:23 +0200

    Completed poker hand identifier

--- Project4-PokerHandDetector/Poker-Hand-Detector.py
+++ Project4-PokerHandDetector/Poker-Hand-Detector.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 import cvzone
 import math
-
+import PokerHandFunction
 
 cap = cv2.VideoCapture(0) #for webcam
 cap.set(3, 640)
@@ -19,16 +19,19 @@ classNames = ['10C', '10D', '10H', '10S',
               '7C', '7D', '7H', '7S', 
               '8C', '8D', '8H', '8S', 
               '9C', '9D', '9H', '9S',
+              'AC', 'AD', 'AH', 'AS',
               'JC', 'JD', 'JH', 'JS',
-              'QC', 'QD', 'QH', 'QS',
               'KC', 'KD', 'KH', 'KS',
-              'AC', 'AD', 'AH', 'AS', 
-
+              'QC', 'QD', 'QH', 'QS'
+              
         ]
+
 
 while True:
     success, img = cap.read()
     results = model(img, stream=True)
+    hand = []
+
     for r in results:
         boxes = r.boxes
         for box in boxes:
@@ -47,8 +50,18 @@ while True:
             #class name
             cls = int(box.cls[0])
 
-            cvzone.putTextRect(img, f'{cls} {conf}', (max(0,x1), max(35,y1)), scale=1, thickness=1)
+            cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0,x1), max(35,y1)), scale=1, thickness=1)
 
+            if conf > 0.5:
+                hand.append(classNames[cls])
+
+    hand = list(set(hand))
+    print(hand)
+
+
+    if(len(hand)==5):
+        results = PokerHandFunction.findPokerHand(hand)
+        cvzone.putTextRect(img, f'Your hand: {results}', (0, 50), scale=2, thickness=3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
